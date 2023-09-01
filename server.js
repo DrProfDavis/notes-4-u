294aa8ea45f3e96a2413d56319d9d0cd775f18c2
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-09-01 11:18:23 -0500

    server.js initial logic

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const htmlRouter = require("./routes/html.js");
+const apiRouter = require("./routes/api.js");
+
+app.use(htmlRouter);
+app.use("/api", apiRouter);
+
+app.listen(PORT, () =>
+    console.log(`Listening at http://localhost:${PORT}`)
+);
