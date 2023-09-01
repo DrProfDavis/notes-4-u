655b9c1320570435f38e80b9bbc8d517cf52cc9e
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-09-01 11:14:58 -0500

    api.js finished

--- routes/api.js
+++ routes/api.js
@@ -26,7 +26,7 @@ router.post('/notes', (req, res) => {
         fs.writeFile(`./db/db.json`, notesString, (err) =>
         err
             ? console.error(err)
-            : console.log(`New note has been added!`) 
+            : console.log(`Note Has Been Added!`) 
         );
 
         const response = {
@@ -36,10 +36,31 @@ router.post('/notes', (req, res) => {
 
         res.status(201).json(response);
     } else {
-        res.status(500).json('Error in adding note');
+        res.status(500).json('Could Not Add Note: Error 500');
     }
 });
 
+router.delete('/notes/:id', (req, res) => {
+    const { id } = req.params;
 
+    fs.readFile("./db/db.json", "utf8", (error, data) =>
+    error ? console.error(error) : (notes = JSON.parse(data))
+    );
+
+    const deletedNote = notes.filter(note => note.id === req.params.id)
+
+    if(deletedNote) {
+        let filteredNotes = notes.filter(note => note.id != req.params.id)
+        let notesString = JSON.stringify(filteredNotes, null, 3);
+        fs.writeFile(`./db/db.json`, notesString, (err) =>
+        err
+        ? console.error(err)
+        : console.log(`Note Has Been Deleted!`));
+
+        res.status(200).json(filteredNotes);
+    } else {
+        res.status(500).json('Could Not Delete Note: Error 500');
+    }
+});
 
 module.exports = router;
